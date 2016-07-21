6899dd19dbe58da6ae65fd157a791151967c16b2
Sky Ao <aoxiaojian@gmail.com>
2016-07-21 15:17:00 +0800

    rollback package name to etcdserverpb, otherwise etcd server will reject the request

---
+++ src/test/java/com/coreos/jetcd/integration/EtcdClientTest.java
@@ -0,0 +1,21 @@
+package com.coreos.jetcd.integration;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class EtcdClientTest {
+    @BeforeMethod
+    public void setUp() throws Exception {
+
+    }
+
+    @AfterMethod
+    public void tearDown() throws Exception {
+
+    }
+
+
+    public void testNone() {
+        System.out.printf("run EtcdClientBuilderTest");
+    }
+}
