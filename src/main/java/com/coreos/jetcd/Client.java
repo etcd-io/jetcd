7c2593ca1b6b2e1340ce219c92970cbc393ab179
fanmin shi <fanmin.shi@coreos.com>
2017-06-02 15:41:07 -0700

    maintenance: add a shared executorService and toChannelAndToken() in client
    the shared executorService can be used by other type of clients and it will be shutdown by close().
    this allows all threads used in other type of client to be managed by the client.
    
    toChannelAndToken() takes in a channelBuilder and returns a channel and token which is
    authenticated by user and password. this allows other type client to create new stub base on
    new channel.

--- src/main/java/com/coreos/jetcd/Client.java
+++ src/main/java/com/coreos/jetcd/Client.java
@@ -7,6 +7,7 @@ import static com.google.common.base.Preconditions.checkNotNull;
 import com.coreos.jetcd.api.AuthGrpc;
 import com.coreos.jetcd.api.AuthenticateRequest;
 import com.coreos.jetcd.api.AuthenticateResponse;
+import com.coreos.jetcd.data.Pair;
 import com.coreos.jetcd.exception.AuthFailedException;
 import com.coreos.jetcd.exception.ConnectException;
 import com.google.common.base.Supplier;
@@ -16,18 +17,16 @@ import com.google.protobuf.ByteString;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 import io.grpc.NameResolver;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * Etcd Client.
  */
 public class Client {
 
-  private final List<String> endpoints;
-  private final ManagedChannel channel;
   private final NameResolver.Factory nameResolverFactory;
   private final Supplier<KV> kvClient;
   private final Supplier<Auth> authClient;
@@ -35,6 +34,25 @@ public class Client {
   private final Supplier<Cluster> clusterClient;
   private final Supplier<Lease> leaseClient;
   private final Supplier<Watch> watchClient;
+  private final ByteString user;
+  private final ByteString pass;
+  private final ManagedChannel channel;
+  private final Optional<String> token;
+
+  // shared executorService
+  ExecutorService executorService = Executors.newCachedThreadPool();
+
+  ExecutorService getExecutorService() {
+    return this.executorService;
+  }
+
+  ManagedChannel getChannel() {
+    return this.channel;
+  }
+
+  Optional<String> getToken() {
+    return this.token;
+  }
 
   public Client(ClientBuilder builder) throws ConnectException, AuthFailedException {
     this(Optional.empty(), builder);
@@ -45,30 +63,41 @@ public class Client {
     this(Optional.ofNullable(channelBuilder), clientBuilder);
   }
 
-  private Client(Optional<ManagedChannelBuilder<?>> channelBuilder,
+  private Client(Optional<ManagedChannelBuilder<?>> channelBuilderOptional,
       ClientBuilder clientBuilder) throws ConnectException, AuthFailedException {
     if (clientBuilder.getNameResolverFactory() != null) {
-      this.endpoints = null;
       this.nameResolverFactory = clientBuilder.getNameResolverFactory();
     } else {
       //If no nameResolverFactory was set, use SimpleEtcdNameResolver
-      this.endpoints = new ArrayList<>(clientBuilder.endpoints());
-      this.nameResolverFactory = ClientUtil.simpleNameResolveFactory(this.endpoints);
+      checkNotNull(clientBuilder.endpoints(), "endpoints can't be null");
+      this.nameResolverFactory = ClientUtil.simpleNameResolveFactory(clientBuilder.endpoints());
     }
+    this.user = clientBuilder.getName();
+    this.pass = clientBuilder.getPassword();
 
-    this.channel = channelBuilder.orElseGet(() -> defaultChannelBuilder(nameResolverFactory))
-        .build();
-
-    Optional<String> token = getToken(channel, clientBuilder);
-
+    Pair<ManagedChannel, Optional<String>> channelToken = this
+        .toChannelAndToken(
+            channelBuilderOptional.orElse(defaultChannelBuilder(nameResolverFactory)));
+    this.channel = channelToken.getKey();
+    this.token = channelToken.getValue();
+    // TODO: *Impl constructor should take client as agrument to its constructor.
     this.kvClient = Suppliers.memoize(() -> new KVImpl(channel, token));
     this.authClient = Suppliers.memoize(() -> new AuthImpl(channel, token));
-    this.maintenanceClient = Suppliers.memoize(() -> new MaintenanceImpl(channel, token));
+    this.maintenanceClient = Suppliers.memoize(() -> new MaintenanceImpl(this));
     this.clusterClient = Suppliers.memoize(() -> new ClusterImpl(channel, token));
     this.leaseClient = Suppliers.memoize(() -> new LeaseImpl(channel, token));
     this.watchClient = Suppliers.memoize(() -> new WatchImpl(channel, token));
   }
 
+  Pair<ManagedChannel, Optional<String>> toChannelAndToken(ManagedChannelBuilder<?> channelBuilder)
+      throws AuthFailedException, ConnectException {
+    checkNotNull(channelBuilder, "channelBuilder can't be null");
+    ManagedChannel managedChannel = channelBuilder.build();
+    Optional<String> token = this.generateToken(managedChannel, this.user, this.pass);
+    return new Pair<>(managedChannel, token);
+  }
+
+  // ************************
   // ************************
   //
   // ************************
@@ -98,7 +127,8 @@ public class Client {
   }
 
   public void close() {
-    channel.shutdownNow();
+    this.executorService.shutdownNow();
+    this.channel.shutdownNow();
   }
 
   // ************************
@@ -130,19 +160,18 @@ public class Client {
    * @throws ConnectException This may be caused as network reason, wrong address
    * @throws AuthFailedException This may be caused as wrong username or password
    */
-  private static Optional<String> getToken(ManagedChannel channel, ClientBuilder builder)
+  private static Optional<String> generateToken(ManagedChannel channel, ByteString user,
+      ByteString pass)
       throws ConnectException, AuthFailedException {
-    if (builder.getName() != null || builder.getPassword() != null) {
-      checkNotNull(builder.getName(), "username can not be null.");
-      checkNotNull(builder.getPassword(), "password can not be null.");
-      checkArgument(builder.getName().toStringUtf8().trim().length() != 0,
-          "username can not be null.");
-      checkArgument(builder.getPassword().toStringUtf8().trim().length() != 0,
-          "password can not be null.");
+    if (user != null && pass != null) {
+      checkArgument(!user.isEmpty(),
+          "username can not be empty.");
+      checkArgument(!pass.isEmpty(),
+          "password can not be empty.");
 
       try {
         return Optional
-            .of(authenticate(channel, builder.getName(), builder.getPassword()).get().getToken());
+            .of(authenticate(channel, user, pass).get().getToken());
       } catch (InterruptedException ite) {
         throw new ConnectException("connect to etcd failed", ite);
       } catch (ExecutionException exee) {
