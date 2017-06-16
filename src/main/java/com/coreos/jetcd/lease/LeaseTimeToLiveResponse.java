493c6e5ebbdabb131a9b583097de04281e2404ad
fanmin shi <fanmin.shi@coreos.com>
2017-06-16 11:31:48 -0700

    Lease: add comments and change getLeaseId() to getID()

--- src/main/java/com/coreos/jetcd/lease/LeaseTimeToLiveResponse.java
+++ src/main/java/com/coreos/jetcd/lease/LeaseTimeToLiveResponse.java
@@ -25,18 +25,31 @@ public class LeaseTimeToLiveResponse {
     return header;
   }
 
-  public long getLeaseId() {
+  /**
+   * ID is the lease ID from the keep alive request.
+   */
+  public long getID() {
     return leaseId;
   }
 
+  /**
+   * TTL is the remaining TTL in seconds for the lease;
+   * the lease will expire in under TTL+1 seconds.
+   */
   public long getTTl() {
     return ttl;
   }
 
+  /**
+   * GrantedTTL is the initial granted time in seconds upon lease creation/renewal.
+   */
   public long getGrantedTTL() {
     return grantedTtl;
   }
 
+  /**
+   * Keys is the list of keys attached to this lease.
+   */
   public List<ByteSequence> getKeys() {
     return keys;
   }
