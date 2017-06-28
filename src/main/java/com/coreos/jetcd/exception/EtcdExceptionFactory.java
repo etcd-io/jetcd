b04a6ce84705a5cf4a9fcbe618f1b8f49fdd78df
fanmin shi <fanmin.shi@coreos.com>
2017-06-28 10:24:11 -0700

    exception: add EtcdException and EtcdExceptionFactory
    EtcdException acts as the base exception for all client related exception.
    It should be thrown if there is client and server error.

---
+++ src/main/java/com/coreos/jetcd/exception/EtcdExceptionFactory.java
@@ -0,0 +1,28 @@
+package com.coreos.jetcd.exception;
+
+/**
+ * A factory for creating instances of {@link EtcdException} and its subtypes.
+ */
+public final class EtcdExceptionFactory {
+
+  public static EtcdException newEtcdException(String message, Throwable cause) {
+    return new EtcdException(message, cause);
+  }
+
+  public static EtcdException newEtcdException(Throwable cause) {
+    return newEtcdException(null, cause);
+  }
+
+  public static EtcdException newEtcdException(String message) {
+    return newEtcdException(message, null);
+  }
+
+  public static CompactedException newCompactedException(long compactedRev) {
+    return new CompactedException(compactedRev, "required revision has been compacted", null);
+  }
+
+  public static EtcdException handleInterrupt(InterruptedException e) {
+    Thread.currentThread().interrupt();
+    return newEtcdException("Interrupted", e);
+  }
+}
