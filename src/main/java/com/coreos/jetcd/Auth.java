5f51f93d23bae76d5cd11ffaa529bb128a5c43d8
fanmin shi <fanmin.shi@coreos.com>
2017-06-30 13:17:06 -0700

    auth: change Auth and AuthImpl to return client side Auth responses

--- src/main/java/com/coreos/jetcd/Auth.java
+++ src/main/java/com/coreos/jetcd/Auth.java
@@ -1,21 +1,21 @@
 package com.coreos.jetcd;
 
-import com.coreos.jetcd.api.AuthDisableResponse;
-import com.coreos.jetcd.api.AuthEnableResponse;
-import com.coreos.jetcd.api.AuthRoleAddResponse;
-import com.coreos.jetcd.api.AuthRoleDeleteResponse;
-import com.coreos.jetcd.api.AuthRoleGetResponse;
-import com.coreos.jetcd.api.AuthRoleGrantPermissionResponse;
-import com.coreos.jetcd.api.AuthRoleListResponse;
-import com.coreos.jetcd.api.AuthRoleRevokePermissionResponse;
-import com.coreos.jetcd.api.AuthUserAddResponse;
-import com.coreos.jetcd.api.AuthUserChangePasswordResponse;
-import com.coreos.jetcd.api.AuthUserDeleteResponse;
-import com.coreos.jetcd.api.AuthUserGetResponse;
-import com.coreos.jetcd.api.AuthUserGrantRoleResponse;
-import com.coreos.jetcd.api.AuthUserListResponse;
-import com.coreos.jetcd.api.AuthUserRevokeRoleResponse;
-import com.coreos.jetcd.api.Permission;
+import com.coreos.jetcd.auth.AuthDisableResponse;
+import com.coreos.jetcd.auth.AuthEnableResponse;
+import com.coreos.jetcd.auth.AuthRoleAddResponse;
+import com.coreos.jetcd.auth.AuthRoleDeleteResponse;
+import com.coreos.jetcd.auth.AuthRoleGetResponse;
+import com.coreos.jetcd.auth.AuthRoleGrantPermissionResponse;
+import com.coreos.jetcd.auth.AuthRoleListResponse;
+import com.coreos.jetcd.auth.AuthRoleRevokePermissionResponse;
+import com.coreos.jetcd.auth.AuthUserAddResponse;
+import com.coreos.jetcd.auth.AuthUserChangePasswordResponse;
+import com.coreos.jetcd.auth.AuthUserDeleteResponse;
+import com.coreos.jetcd.auth.AuthUserGetResponse;
+import com.coreos.jetcd.auth.AuthUserGrantRoleResponse;
+import com.coreos.jetcd.auth.AuthUserListResponse;
+import com.coreos.jetcd.auth.AuthUserRevokeRoleResponse;
+import com.coreos.jetcd.auth.Permission;
 import com.coreos.jetcd.data.ByteSequence;
 import com.coreos.jetcd.internal.impl.CloseableClient;
 import java.util.concurrent.CompletableFuture;
@@ -25,56 +25,85 @@ import java.util.concurrent.CompletableFuture;
  */
 public interface Auth extends CloseableClient {
 
-  // ***************
-  // Auth Manage
-  // ***************
-
+  /**
+   * enables auth of an etcd cluster.
+   */
   CompletableFuture<AuthEnableResponse> authEnable();
 
+  /**
+   * disables auth of an etcd cluster.
+   */
   CompletableFuture<AuthDisableResponse> authDisable();
 
-  // ***************
-  // User Manage
-  // ***************
+  /**
+   * adds a new user to an etcd cluster.
+   */
+  CompletableFuture<AuthUserAddResponse> userAdd(ByteSequence user, ByteSequence password);
 
-  CompletableFuture<AuthUserAddResponse> userAdd(ByteSequence name, ByteSequence password);
+  /**
+   * deletes a user from an etcd cluster.
+   */
+  CompletableFuture<AuthUserDeleteResponse> userDelete(ByteSequence user);
 
-  CompletableFuture<AuthUserDeleteResponse> userDelete(ByteSequence name);
-
-  CompletableFuture<AuthUserChangePasswordResponse> userChangePassword(ByteSequence name,
+  /**
+   * changes a password of a user.
+   */
+  CompletableFuture<AuthUserChangePasswordResponse> userChangePassword(ByteSequence user,
       ByteSequence password);
 
-  CompletableFuture<AuthUserGetResponse> userGet(ByteSequence name);
+  /**
+   * gets a detailed information of a user.
+   */
+  CompletableFuture<AuthUserGetResponse> userGet(ByteSequence user);
 
+  /**
+   * gets a list of all users.
+   */
   CompletableFuture<AuthUserListResponse> userList();
 
-  // ***************
-  // User Role Manage
-  // ***************
+  /**
+   * grants a role to a user.
+   */
+  CompletableFuture<AuthUserGrantRoleResponse> userGrantRole(ByteSequence user, ByteSequence role);
 
-  CompletableFuture<AuthUserGrantRoleResponse> userGrantRole(ByteSequence name, ByteSequence role);
-
-  CompletableFuture<AuthUserRevokeRoleResponse> userRevokeRole(ByteSequence name,
+  /**
+   * revokes a role of a user.
+   */
+  CompletableFuture<AuthUserRevokeRoleResponse> userRevokeRole(ByteSequence user,
       ByteSequence role);
 
-  // ***************
-  // Role Manage
-  // ***************
+  /**
+   * adds a new role to an etcd cluster.
+   */
+  CompletableFuture<AuthRoleAddResponse> roleAdd(ByteSequence user);
 
-  CompletableFuture<AuthRoleAddResponse> roleAdd(ByteSequence name);
-
+  /**
+   * grants a permission to a role.
+   */
   CompletableFuture<AuthRoleGrantPermissionResponse> roleGrantPermission(ByteSequence role,
       ByteSequence key,
       ByteSequence rangeEnd, Permission.Type permType);
 
+  /**
+   * gets a detailed information of a role.
+   */
   CompletableFuture<AuthRoleGetResponse> roleGet(ByteSequence role);
 
+  /**
+   * gets a list of all roles.
+   */
   CompletableFuture<AuthRoleListResponse> roleList();
 
+  /**
+   * revokes a permission from a role.
+   */
   CompletableFuture<AuthRoleRevokePermissionResponse> roleRevokePermission(ByteSequence role,
       ByteSequence key,
       ByteSequence rangeEnd);
 
+  /**
+   * RoleDelete deletes a role.
+   */
   CompletableFuture<AuthRoleDeleteResponse> roleDelete(ByteSequence role);
 
 }
--- src/main/java/com/coreos/jetcd/internal/impl/AuthImpl.java
+++ src/main/java/com/coreos/jetcd/internal/impl/AuthImpl.java
@@ -1,185 +1,254 @@
 package com.coreos.jetcd.internal.impl;
 
+import static com.google.common.base.Preconditions.checkNotNull;
+
 import com.coreos.jetcd.Auth;
 import com.coreos.jetcd.api.AuthDisableRequest;
-import com.coreos.jetcd.api.AuthDisableResponse;
 import com.coreos.jetcd.api.AuthEnableRequest;
-import com.coreos.jetcd.api.AuthEnableResponse;
 import com.coreos.jetcd.api.AuthGrpc;
 import com.coreos.jetcd.api.AuthRoleAddRequest;
-import com.coreos.jetcd.api.AuthRoleAddResponse;
 import com.coreos.jetcd.api.AuthRoleDeleteRequest;
-import com.coreos.jetcd.api.AuthRoleDeleteResponse;
 import com.coreos.jetcd.api.AuthRoleGetRequest;
-import com.coreos.jetcd.api.AuthRoleGetResponse;
 import com.coreos.jetcd.api.AuthRoleGrantPermissionRequest;
-import com.coreos.jetcd.api.AuthRoleGrantPermissionResponse;
 import com.coreos.jetcd.api.AuthRoleListRequest;
-import com.coreos.jetcd.api.AuthRoleListResponse;
 import com.coreos.jetcd.api.AuthRoleRevokePermissionRequest;
-import com.coreos.jetcd.api.AuthRoleRevokePermissionResponse;
 import com.coreos.jetcd.api.AuthUserAddRequest;
-import com.coreos.jetcd.api.AuthUserAddResponse;
 import com.coreos.jetcd.api.AuthUserChangePasswordRequest;
-import com.coreos.jetcd.api.AuthUserChangePasswordResponse;
 import com.coreos.jetcd.api.AuthUserDeleteRequest;
-import com.coreos.jetcd.api.AuthUserDeleteResponse;
 import com.coreos.jetcd.api.AuthUserGetRequest;
-import com.coreos.jetcd.api.AuthUserGetResponse;
 import com.coreos.jetcd.api.AuthUserGrantRoleRequest;
-import com.coreos.jetcd.api.AuthUserGrantRoleResponse;
 import com.coreos.jetcd.api.AuthUserListRequest;
-import com.coreos.jetcd.api.AuthUserListResponse;
 import com.coreos.jetcd.api.AuthUserRevokeRoleRequest;
-import com.coreos.jetcd.api.AuthUserRevokeRoleResponse;
-import com.coreos.jetcd.api.Permission;
+import com.coreos.jetcd.api.Permission.Type;
+import com.coreos.jetcd.auth.AuthDisableResponse;
+import com.coreos.jetcd.auth.AuthEnableResponse;
+import com.coreos.jetcd.auth.AuthRoleAddResponse;
+import com.coreos.jetcd.auth.AuthRoleDeleteResponse;
+import com.coreos.jetcd.auth.AuthRoleGetResponse;
+import com.coreos.jetcd.auth.AuthRoleGrantPermissionResponse;
+import com.coreos.jetcd.auth.AuthRoleListResponse;
+import com.coreos.jetcd.auth.AuthRoleRevokePermissionResponse;
+import com.coreos.jetcd.auth.AuthUserAddResponse;
+import com.coreos.jetcd.auth.AuthUserChangePasswordResponse;
+import com.coreos.jetcd.auth.AuthUserDeleteResponse;
+import com.coreos.jetcd.auth.AuthUserGetResponse;
+import com.coreos.jetcd.auth.AuthUserGrantRoleResponse;
+import com.coreos.jetcd.auth.AuthUserListResponse;
+import com.coreos.jetcd.auth.AuthUserRevokeRoleResponse;
+import com.coreos.jetcd.auth.Permission;
 import com.coreos.jetcd.data.ByteSequence;
 import java.util.concurrent.CompletableFuture;
-import net.javacrumbs.futureconverter.java8guava.FutureConverter;
 
 /**
  * Implementation of etcd auth client.
  */
 class AuthImpl implements Auth {
+
   private final AuthGrpc.AuthFutureStub stub;
+  private final ClientConnectionManager connectionManager;
 
   AuthImpl(ClientConnectionManager connectionManager) {
+    this.connectionManager = connectionManager;
     this.stub = connectionManager.newStub(AuthGrpc::newFutureStub);
   }
-
-  // ***************
-  // Auth Manage
-  // ***************
 
   @Override
   public CompletableFuture<AuthEnableResponse> authEnable() {
     AuthEnableRequest enableRequest = AuthEnableRequest.getDefaultInstance();
-    return FutureConverter.toCompletableFuture(this.stub.authEnable(enableRequest));
+    return Util.listenableToCompletableFuture(
+        this.stub.authEnable(enableRequest),
+        Util::toAuthEnableResponse,
+        this.connectionManager.getExecutorService());
   }
 
   @Override
   public CompletableFuture<AuthDisableResponse> authDisable() {
     AuthDisableRequest disableRequest = AuthDisableRequest.getDefaultInstance();
-    return FutureConverter.toCompletableFuture(this.stub.authDisable(disableRequest));
+    return Util.listenableToCompletableFuture(
+        this.stub.authDisable(disableRequest),
+        Util::toAuthDisableResponse,
+        this.connectionManager.getExecutorService());
   }
 
-  // ***************
-  // User Manage
-  // ***************
-
   @Override
-  public CompletableFuture<AuthUserAddResponse> userAdd(ByteSequence name, ByteSequence password) {
+  public CompletableFuture<AuthUserAddResponse> userAdd(ByteSequence user, ByteSequence password) {
+    checkNotNull(user, "user can't be null");
+    checkNotNull(password, "password can't be null");
+
     AuthUserAddRequest addRequest = AuthUserAddRequest.newBuilder()
-        .setNameBytes(Util.byteStringFromByteSequence(name))
+        .setNameBytes(Util.byteStringFromByteSequence(user))
         .setPasswordBytes(Util.byteStringFromByteSequence(password))
         .build();
-    return FutureConverter.toCompletableFuture(this.stub.userAdd(addRequest));
+    return Util.listenableToCompletableFuture(
+        this.stub.userAdd(addRequest),
+        Util::toAuthUserAddResponse,
+        this.connectionManager.getExecutorService());
   }
 
   @Override
-  public CompletableFuture<AuthUserDeleteResponse> userDelete(ByteSequence name) {
+  public CompletableFuture<AuthUserDeleteResponse> userDelete(ByteSequence user) {
+    checkNotNull(user, "user can't be null");
+
     AuthUserDeleteRequest deleteRequest = AuthUserDeleteRequest.newBuilder()
-        .setNameBytes(Util.byteStringFromByteSequence(name))
+        .setNameBytes(Util.byteStringFromByteSequence(user))
         .build();
-    return FutureConverter.toCompletableFuture(this.stub.userDelete(deleteRequest));
+    return Util.listenableToCompletableFuture(
+        this.stub.userDelete(deleteRequest),
+        Util::toAuthUserDeleteResponse,
+        this.connectionManager.getExecutorService());
   }
 
   @Override
-  public CompletableFuture<AuthUserChangePasswordResponse> userChangePassword(ByteSequence name,
+  public CompletableFuture<AuthUserChangePasswordResponse> userChangePassword(ByteSequence user,
       ByteSequence password) {
+    checkNotNull(user, "user can't be null");
+    checkNotNull(password, "password can't be null");
+
     AuthUserChangePasswordRequest changePasswordRequest = AuthUserChangePasswordRequest.newBuilder()
-        .setNameBytes(Util.byteStringFromByteSequence(name))
+        .setNameBytes(Util.byteStringFromByteSequence(user))
         .setPasswordBytes(Util.byteStringFromByteSequence(password))
         .build();
-    return FutureConverter.toCompletableFuture(this.stub.userChangePassword(changePasswordRequest));
+    return Util.listenableToCompletableFuture(
+        this.stub.userChangePassword(changePasswordRequest),
+        Util::toAuthUserChangePasswordResponse,
+        this.connectionManager.getExecutorService());
   }
 
   @Override
-  public CompletableFuture<AuthUserGetResponse> userGet(ByteSequence name) {
-    AuthUserGetRequest userGetRequest = AuthUserGetRequest.newBuilder()
-        .setNameBytes(Util.byteStringFromByteSequence(name))
-        .build();
+  public CompletableFuture<AuthUserGetResponse> userGet(ByteSequence user) {
+    checkNotNull(user, "user can't be null");
 
-    return FutureConverter.toCompletableFuture(this.stub.userGet(userGetRequest));
+    AuthUserGetRequest userGetRequest = AuthUserGetRequest.newBuilder()
+        .setNameBytes(Util.byteStringFromByteSequence(user))
+        .build();
+    return Util.listenableToCompletableFuture(
+        this.stub.userGet(userGetRequest),
+        Util::toAuthUserGetResponse,
+        this.connectionManager.getExecutorService());
   }
 
   @Override
   public CompletableFuture<AuthUserListResponse> userList() {
     AuthUserListRequest userListRequest = AuthUserListRequest.getDefaultInstance();
-    return FutureConverter.toCompletableFuture(this.stub.userList(userListRequest));
+    return Util.listenableToCompletableFuture(
+        this.stub.userList(userListRequest),
+        Util::toAuthUserListResponse,
+        this.connectionManager.getExecutorService());
   }
 
-  // ***************
-  // User Role Manage
-  // ***************
-
   @Override
-  public CompletableFuture<AuthUserGrantRoleResponse> userGrantRole(ByteSequence name,
+  public CompletableFuture<AuthUserGrantRoleResponse> userGrantRole(ByteSequence user,
       ByteSequence role) {
+    checkNotNull(user, "user can't be null");
+    checkNotNull(role, "key can't be null");
+
     AuthUserGrantRoleRequest userGrantRoleRequest = AuthUserGrantRoleRequest.newBuilder()
-        .setUserBytes(Util.byteStringFromByteSequence(name))
+        .setUserBytes(Util.byteStringFromByteSequence(user))
         .setRoleBytes(Util.byteStringFromByteSequence(role))
         .build();
-    return FutureConverter.toCompletableFuture(this.stub.userGrantRole(userGrantRoleRequest));
+    return Util.listenableToCompletableFuture(
+        this.stub.userGrantRole(userGrantRoleRequest),
+        Util::toAuthUserGrantRoleResponse,
+        this.connectionManager.getExecutorService());
   }
 
   @Override
-  public CompletableFuture<AuthUserRevokeRoleResponse> userRevokeRole(ByteSequence name,
+  public CompletableFuture<AuthUserRevokeRoleResponse> userRevokeRole(ByteSequence user,
       ByteSequence role) {
+    checkNotNull(user, "user can't be null");
+    checkNotNull(role, "key can't be null");
+
     AuthUserRevokeRoleRequest userRevokeRoleRequest = AuthUserRevokeRoleRequest.newBuilder()
-        .setNameBytes(Util.byteStringFromByteSequence(name))
+        .setNameBytes(Util.byteStringFromByteSequence(user))
         .setRoleBytes(Util.byteStringFromByteSequence(role))
         .build();
-    return FutureConverter.toCompletableFuture(this.stub.userRevokeRole(userRevokeRoleRequest));
+    return Util.listenableToCompletableFuture(
+        this.stub.userRevokeRole(userRevokeRoleRequest),
+        Util::toAuthUserRevokeRoleResponse,
+        this.connectionManager.getExecutorService());
   }
 
-  // ***************
-  // Role Manage
-  // ***************
-
   @Override
-  public CompletableFuture<AuthRoleAddResponse> roleAdd(ByteSequence name) {
+  public CompletableFuture<AuthRoleAddResponse> roleAdd(ByteSequence user) {
+    checkNotNull(user, "user can't be null");
+
     AuthRoleAddRequest roleAddRequest = AuthRoleAddRequest.newBuilder()
-        .setNameBytes(Util.byteStringFromByteSequence(name))
+        .setNameBytes(Util.byteStringFromByteSequence(user))
         .build();
-    return FutureConverter.toCompletableFuture(this.stub.roleAdd(roleAddRequest));
+    return Util.listenableToCompletableFuture(
+        this.stub.roleAdd(roleAddRequest),
+        Util::toAuthRoleAddResponse,
+        this.connectionManager.getExecutorService());
   }
 
   @Override
   public CompletableFuture<AuthRoleGrantPermissionResponse> roleGrantPermission(ByteSequence role,
       ByteSequence key, ByteSequence rangeEnd, Permission.Type permType) {
-    Permission perm = Permission.newBuilder()
+    checkNotNull(role, "role can't be null");
+    checkNotNull(key, "key can't be null");
+    checkNotNull(rangeEnd, "rangeEnd can't be null");
+    checkNotNull(permType, "permType can't be null");
+
+    com.coreos.jetcd.api.Permission.Type type;
+    switch (permType) {
+      case WRITE:
+        type = Type.WRITE;
+        break;
+      case READWRITE:
+        type = Type.READWRITE;
+        break;
+      case READ:
+        type = Type.READ;
+        break;
+      default:
+        type = Type.UNRECOGNIZED;
+        break;
+    }
+
+    com.coreos.jetcd.api.Permission perm = com.coreos.jetcd.api.Permission.newBuilder()
         .setKey(Util.byteStringFromByteSequence(key))
         .setRangeEnd(Util.byteStringFromByteSequence(rangeEnd))
-        .setPermType(permType)
+        .setPermType(type)
         .build();
     AuthRoleGrantPermissionRequest roleGrantPermissionRequest = AuthRoleGrantPermissionRequest
         .newBuilder()
         .setNameBytes(Util.byteStringFromByteSequence(role))
         .setPerm(perm)
         .build();
-
-    return FutureConverter
-        .toCompletableFuture(this.stub.roleGrantPermission(roleGrantPermissionRequest));
+    return Util.listenableToCompletableFuture(
+        this.stub.roleGrantPermission(roleGrantPermissionRequest),
+        Util::toAuthRoleGrantPermissionResponse,
+        this.connectionManager.getExecutorService());
   }
 
   @Override
   public CompletableFuture<AuthRoleGetResponse> roleGet(ByteSequence role) {
+    checkNotNull(role, "role can't be null");
+
     AuthRoleGetRequest roleGetRequest = AuthRoleGetRequest.newBuilder()
         .setRoleBytes(Util.byteStringFromByteSequence(role))
         .build();
-    return FutureConverter.toCompletableFuture(this.stub.roleGet(roleGetRequest));
+    return Util.listenableToCompletableFuture(
+        this.stub.roleGet(roleGetRequest),
+        Util::toAuthRoleGetResponse,
+        this.connectionManager.getExecutorService());
   }
 
   @Override
   public CompletableFuture<AuthRoleListResponse> roleList() {
     AuthRoleListRequest roleListRequest = AuthRoleListRequest.getDefaultInstance();
-    return FutureConverter.toCompletableFuture(this.stub.roleList(roleListRequest));
+    return Util.listenableToCompletableFuture(
+        this.stub.roleList(roleListRequest),
+        Util::toAuthRoleListResponse,
+        this.connectionManager.getExecutorService());
   }
 
   @Override
   public CompletableFuture<AuthRoleRevokePermissionResponse> roleRevokePermission(ByteSequence role,
       ByteSequence key, ByteSequence rangeEnd) {
+    checkNotNull(role, "role can't be null");
+    checkNotNull(key, "key can't be null");
+    checkNotNull(rangeEnd, "rangeEnd can't be null");
 
     AuthRoleRevokePermissionRequest roleRevokePermissionRequest = AuthRoleRevokePermissionRequest
         .newBuilder()
@@ -187,17 +256,22 @@ class AuthImpl implements Auth {
         .setKeyBytes(Util.byteStringFromByteSequence(key))
         .setRangeEndBytes(Util.byteStringFromByteSequence(rangeEnd))
         .build();
-    return FutureConverter
-        .toCompletableFuture(this.stub.roleRevokePermission(roleRevokePermissionRequest));
+    return Util
+        .listenableToCompletableFuture(
+            this.stub.roleRevokePermission(roleRevokePermissionRequest),
+            Util::toAuthRoleRevokePermissionResponse,
+            this.connectionManager.getExecutorService());
   }
 
   @Override
   public CompletableFuture<AuthRoleDeleteResponse> roleDelete(ByteSequence role) {
-
+    checkNotNull(role, "role can't be null");
     AuthRoleDeleteRequest roleDeleteRequest = AuthRoleDeleteRequest.newBuilder()
         .setRoleBytes(Util.byteStringFromByteSequence(role))
         .build();
-    return FutureConverter.toCompletableFuture(this.stub.roleDelete(roleDeleteRequest));
+    return Util.listenableToCompletableFuture(
+        this.stub.roleDelete(roleDeleteRequest),
+        Util::toAuthRoleDeleteResponse,
+        this.connectionManager.getExecutorService());
   }
-
 }
