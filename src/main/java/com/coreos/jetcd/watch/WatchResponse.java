56b06027f455b75b2644f4407faddbf3b29adc1e
fanmin shi <fanmin.shi@coreos.com>
2017-07-18 15:49:23 -0700

    watch: let watch responses extend AbstractResponse

--- src/main/java/com/coreos/jetcd/watch/WatchResponse.java
+++ src/main/java/com/coreos/jetcd/watch/WatchResponse.java
@@ -1,23 +1,49 @@
 package com.coreos.jetcd.watch;
 
-import com.coreos.jetcd.data.Header;
+import com.coreos.jetcd.api.Event;
+import com.coreos.jetcd.data.AbstractResponse;
+import com.coreos.jetcd.data.KeyValue;
 import java.util.List;
+import java.util.stream.Collectors;
 
-public class WatchResponse {
+public class WatchResponse extends AbstractResponse<com.coreos.jetcd.api.WatchResponse> {
 
-  private Header header;
   private List<WatchEvent> events;
 
-  public WatchResponse(Header header, List<WatchEvent> events) {
-    this.header = header;
-    this.events = events;
+  public WatchResponse(com.coreos.jetcd.api.WatchResponse response) {
+    super(response, response.getHeader());
   }
 
-  public Header getHeader() {
-    return header;
+  /**
+   * convert API watch event to client event.
+   */
+  private static WatchEvent toEvent(Event event) {
+    WatchEvent.EventType eventType;
+    switch (event.getType()) {
+      case DELETE:
+        eventType = WatchEvent.EventType.DELETE;
+        break;
+      case PUT:
+        eventType = WatchEvent.EventType.PUT;
+        break;
+      default:
+        eventType = WatchEvent.EventType.UNRECOGNIZED;
+    }
+
+    return new WatchEvent(
+        new KeyValue(event.getKv()),
+        new KeyValue(event.getPrevKv()),
+        eventType);
   }
 
-  public List<WatchEvent> getEvents() {
+  public synchronized List<WatchEvent> getEvents() {
+    if (events == null) {
+      events = getResponse().getEventsList().stream()
+          .map(WatchResponse::toEvent).collect(
+              Collectors.toList());
+
+    }
+
     return events;
   }
 }
