88dece311c7a92665260534c7a56e1ef112c1359
fanmin shi <fanmin.shi@coreos.com>
2017-07-18 15:48:47 -0700

    maintenance: let maintenance responses extends AbstractResponse

--- src/main/java/com/coreos/jetcd/maintenance/AlarmResponse.java
+++ src/main/java/com/coreos/jetcd/maintenance/AlarmResponse.java
@@ -1,31 +1,47 @@
 package com.coreos.jetcd.maintenance;
 
 import com.coreos.jetcd.Maintenance;
-import com.coreos.jetcd.data.Header;
+import com.coreos.jetcd.data.AbstractResponse;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * AlarmResponse returned by {@link Maintenance#listAlarms()} contains a header
  * and a list of AlarmMember.
  */
-public class AlarmResponse {
+public class AlarmResponse extends AbstractResponse<com.coreos.jetcd.api.AlarmResponse> {
 
-  private final Header header;
-  private final List<AlarmMember> alarms;
+  private List<AlarmMember> alarms;
 
-  public AlarmResponse(Header header, List<AlarmMember> alarms) {
-    this.header = header;
-    this.alarms = alarms;
+  public AlarmResponse(com.coreos.jetcd.api.AlarmResponse response) {
+    super(response, response.getHeader());
   }
 
-  public Header getHeader() {
-    return header;
+  private static AlarmMember toAlarmMember(com.coreos.jetcd.api.AlarmMember alarmMember) {
+    com.coreos.jetcd.maintenance.AlarmType type;
+    switch (alarmMember.getAlarm()) {
+      case NONE:
+        type = com.coreos.jetcd.maintenance.AlarmType.NONE;
+        break;
+      case NOSPACE:
+        type = com.coreos.jetcd.maintenance.AlarmType.NOSPACE;
+        break;
+      default:
+        type = com.coreos.jetcd.maintenance.AlarmType.UNRECOGNIZED;
+    }
+    return new AlarmMember(alarmMember.getMemberID(), type);
   }
 
   /**
    * returns a list of alarms associated with the alarm request.
    */
-  public List<AlarmMember> getAlarms() {
+  public synchronized List<AlarmMember> getAlarms() {
+    if (alarms == null) {
+      alarms = getResponse().getAlarmsList().stream()
+          .map(AlarmResponse::toAlarmMember)
+          .collect(Collectors.toList());
+    }
+
     return alarms;
   }
 }
