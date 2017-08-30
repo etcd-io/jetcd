ec10311d859f20c909acfa33db887263c8b68605
fanmin shi <fanmin.shi@coreos.com>
2017-08-30 11:00:14 -0700

    ErrorCode: credit google for ErrorCode.java

--- jetcd-core/src/main/java/com/coreos/jetcd/exception/ErrorCode.java
+++ jetcd-core/src/main/java/com/coreos/jetcd/exception/ErrorCode.java
@@ -1,11 +1,11 @@
 /**
- * Copyright 2017 The jetcd authors
+ * Copyright 2017 Google Inc. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *       http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -21,6 +21,12 @@ import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+/**
+ * ErrorCode is a wrapper around grpc Error code.
+ *
+ * <P>Modification Notice:
+ * This is a modification of ErrorCode.java from Google-cloud-spanner java api.
+ */
 public enum ErrorCode {
 
   CANCELLED(Status.CANCELLED),
