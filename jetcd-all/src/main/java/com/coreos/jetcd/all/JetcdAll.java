f28af18ab092054e1d472d42cebc34089ea4c716
lburgazzoli <lburgazzoli@gmail.com>
2018-04-09 11:32:48 +0200

    chore(build): generate javadoc whilde building jetcd-all

--- jetcd-all/src/main/java/com/coreos/jetcd/all/JetcdAll.java
+++ jetcd-all/src/main/java/com/coreos/jetcd/all/JetcdAll.java
@@ -13,5 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.coreos.jetcd.all;
+
+public final class JetcdAll {    
+    private JetcdAll() {        
+    }
+}
