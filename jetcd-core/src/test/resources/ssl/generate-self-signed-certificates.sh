0e2191ca3093606dfe7c19717b56ba0ef1ae7b62
Luca Burgazzoli <lburgazzoli@gmail.com>
2023-05-28 15:55:49 +0200

    chore(test): re-generate ssl certs

--- jetcd-core/src/test/resources/ssl/generate-self-signed-certificates.sh
+++ jetcd-core/src/test/resources/ssl/generate-self-signed-certificates.sh
@@ -17,41 +17,43 @@
 
 export SCRIPT_PATH=$(dirname "$(readlink -f "$0")")
 export ROOT=$SCRIPT_PATH
-export CFSSL_HOME=$ROOT/cfssl
 export CERT_HOME=$ROOT/cert/
 
-mkdir -p $CFSSL_HOME
-mkdir -p $CERT_HOME
+mkdir -p "$CERT_HOME"
+cd "$CERT_HOME" || exit
 
-curl -s -L -o $CFSSL_HOME/cfssl https://pkg.cfssl.org/R1.2/cfssl_linux-amd64
-curl -s -L -o $CFSSL_HOME/cfssljson https://pkg.cfssl.org/R1.2/cfssljson_linux-amd64
-chmod +x $CFSSL_HOME/{cfssl,cfssljson}
-
-
-cd $CERT_HOME
-
-echo '{"CN":"CA","key":{"algo":"rsa","size":2048}}' | $CFSSL_HOME/cfssl gencert -initca - | $CFSSL_HOME/cfssljson -bare ca -
+echo '{"CN":"CA","key":{"algo":"rsa","size":2048}}' | cfssl gencert -initca - | cfssljson -bare ca -
 echo '{"signing":{"default":{"expiry":"43800h","usages":["signing","key encipherment","server auth","client auth"]}}}' > ca-config.json
 
 export ADDRESS=etcd-ssl
 export NAME=server
-echo '{"CN":"'$NAME'","hosts":[""],"key":{"algo":"rsa","size":2048}}' | $CFSSL_HOME/cfssl gencert -config=ca-config.json -ca=ca.pem -ca-key=ca-key.pem -hostname="$ADDRESS" - | $CFSSL_HOME/cfssljson -bare $NAME
+echo '{"CN":"'$NAME'","hosts":[""],"key":{"algo":"rsa","size":2048}}' | \
+    cfssl gencert -config=ca-config.json -ca=ca.pem -ca-key=ca-key.pem -hostname="$ADDRESS" - | \
+    cfssljson -bare $NAME
 
 
 export ADDRESS=etcd0
 export NAME=etcd0
-echo '{"CN":"'$NAME'","hosts":[""],"key":{"algo":"rsa","size":2048}}' | $CFSSL_HOME/cfssl gencert -config=ca-config.json -ca=ca.pem -ca-key=ca-key.pem -hostname="$ADDRESS" - | $CFSSL_HOME/cfssljson -bare $NAME
+echo '{"CN":"'$NAME'","hosts":[""],"key":{"algo":"rsa","size":2048}}' | \
+    cfssl gencert -config=ca-config.json -ca=ca.pem -ca-key=ca-key.pem -hostname="$ADDRESS" - | \
+    cfssljson -bare $NAME
 
 
 export ADDRESS=etcd1
 export NAME=etcd1
-echo '{"CN":"'$NAME'","hosts":[""],"key":{"algo":"rsa","size":2048}}' | $CFSSL_HOME/cfssl gencert -config=ca-config.json -ca=ca.pem -ca-key=ca-key.pem -hostname="$ADDRESS" - | $CFSSL_HOME/cfssljson -bare $NAME
+echo '{"CN":"'$NAME'","hosts":[""],"key":{"algo":"rsa","size":2048}}' | \
+    cfssl gencert -config=ca-config.json -ca=ca.pem -ca-key=ca-key.pem -hostname="$ADDRESS" - | \
+    cfssljson -bare $NAME
 
 
 export ADDRESS=etcd2
 export NAME=etcd2
-echo '{"CN":"'$NAME'","hosts":[""],"key":{"algo":"rsa","size":2048}}' | $CFSSL_HOME/cfssl gencert -config=ca-config.json -ca=ca.pem -ca-key=ca-key.pem -hostname="$ADDRESS" - | $CFSSL_HOME/cfssljson -bare $NAME
+echo '{"CN":"'$NAME'","hosts":[""],"key":{"algo":"rsa","size":2048}}' | \
+    cfssl gencert -config=ca-config.json -ca=ca.pem -ca-key=ca-key.pem -hostname="$ADDRESS" - | \
+    cfssljson -bare $NAME
 
 export ADDRESS=
 export NAME=client
-echo '{"CN":"'$NAME'","hosts":[""],"key":{"algo":"rsa","size":2048}}' | $CFSSL_HOME/cfssl gencert -config=ca-config.json -ca=ca.pem -ca-key=ca-key.pem -hostname="$ADDRESS" - | $CFSSL_HOME/cfssljson -bare $NAME
+echo '{"CN":"'$NAME'","hosts":[""],"key":{"algo":"rsa","size":2048}}' | \
+    cfssl gencert -config=ca-config.json -ca=ca.pem -ca-key=ca-key.pem -hostname="$ADDRESS" - | \
+    cfssljson -bare $NAME
